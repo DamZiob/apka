0aac9f394e04dcb064e31297078adfd8bbb5cbdc
DamZiob <82330424+DamZiob@users.noreply.github.com>
2021-04-19 17:54:41 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,13 @@
         <b-col class="menu" cols="6" offset="3">
           <div class="name">Perkusja</div>
           <label class="vol" for="range-1">Volume</label>
-    <b-form-input id="range-1" v-model="value" type="range" min="0" max="5"></b-form-input>
+          <b-form-input
+            id="range-1"
+            v-model="value"
+            type="range"
+            min="0"
+            max="5"
+          ></b-form-input>
         </b-col>
         <b-col class="col" cols="3" offset="3">
           <div class="section">
@@ -13,9 +19,15 @@
               class="button btnFirst"
               v-for="snare in snares"
               :key="snare.id"
-              @click="playSnares(snare.url)">
+              @click="playSnares(snare.url)"
+            >
               {{ snare.id }}
             </div>
+          </div>
+          <div class="switch">
+            <b-form-checkbox v-model="checked1" name="check-button" switch>
+              Loop snares
+            </b-form-checkbox>
           </div>
         </b-col>
         <b-col class="col" cols="3" offset="0">
@@ -24,18 +36,25 @@
               class="button btnSecond"
               v-for="bass in basses"
               :key="bass.id"
-              @click="playBasses(bass.url)">
+              @click="playBasses(bass.url)"
+            >
               {{ bass.id }}
             </div>
+          </div>
+          <div class="switch">
+            <b-form-checkbox v-model="checked2" name="check-button" switch>
+              Loop basses
+            </b-form-checkbox>
           </div>
         </b-col>
         <b-col class="col second" cols="3" offset="3">
           <div class="section">
             <div
-              class="button btnThird "
+              class="button btnThird"
               v-for="hihat in hihats"
               :key="hihat.id"
-              @click="playHihats(hihat.url)">
+              @click="playHihats(hihat.url)"
+            >
               {{ hihat.id }}
             </div>
           </div>
@@ -46,7 +65,8 @@
               class="button btnFourth"
               v-for="waterdrum in waterdrums"
               :key="waterdrum.id"
-              @click="playWaterdrums(waterdrum.url)">
+              @click="playWaterdrums(waterdrum.url)"
+            >
               {{ waterdrum.id }}
             </div>
           </div>
@@ -71,30 +91,49 @@ export default {
       snares,
       basses,
       hihats,
-      value:3
+      value: 3,
+      checked1: false,
+      checked2: false,
     };
   },
   methods: {
     playSnares(snares) {
-      let audio = new Audio(snares);
-      audio.volume=this.value /5;
-      audio.play();
+      if (this.checked1) {
+        let audio = new Audio(snares);
+        audio.loop = true;
+        audio.playbackRate = 2;
+        audio.volume = this.value / 5;
+        audio.play();
+      } else {
+        let audio = new Audio(snares);
+        audio.loop = false;
+        audio.volume = this.value / 5;
+        audio.play();
+      }
     },
     playBasses(basses) {
-      let audio = new Audio(basses);
-      audio.volume=this.value /5;
-      audio.play();
+      if (this.checked2) {
+        let audio = new Audio(basses);
+        audio.loop = true;
+        audio.playbackRate = 2;
+        audio.volume = this.value / 5;
+        audio.play();
+      } else {
+        let audio = new Audio(basses);
+        audio.loop = false;
+        audio.volume = this.value / 5;
+        audio.play();
+      }
     },
     playHihats(hihats) {
       let audio = new Audio(hihats);
-     audio.volume=this.value /5;
+      audio.volume = this.value / 5;
       audio.play();
     },
     playWaterdrums(waterdrums) {
       let audio = new Audio(waterdrums);
-      audio.volume=this.value /5;
+      audio.volume = this.value / 5;
       audio.play();
-      
     },
   },
 };
